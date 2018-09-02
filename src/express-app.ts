9612d29516ce9793ff30969db78704e12c2fefbe
Maakep <schon.fredrik@gmail.com>
2018-09-02 14:29:53 +0200

    Add endpoint and handle for Slack Challenge

--- src/express-app.ts
+++ src/express-app.ts
@@ -6,4 +6,9 @@ const app: express.Express = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.post('/', (req, res) => {
+    res.send(req.body["challenge"]);
+    console.log(req.body["challenge"]);
+});
+
 export default app;
